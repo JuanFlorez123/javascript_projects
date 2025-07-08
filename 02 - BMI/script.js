38fca3b137ffeb594879c431dd2d31dc0357677c
JuanFlorez123 <63020194+JuanFlorez123@users.noreply.github.com>
2025-07-08 10:11:53 -0500

    Carga de proyectos JS

---
+++ 02 - BMI/script.js
@@ -0,0 +1,12 @@
+function calculateBMI() {
+    const weight = parseFloat(document.getElementById("weight").value);
+    const height = parseFloat(document.getElementById("height").value)/ 100;
+    
+    if((weight && height) && weight > 0 && height > 0) {
+        const bmi = (weight / (height * height)).toFixed(2);
+        document.getElementById("result").textContent = `Tu BMI es: ${bmi}`;
+    } else {
+        document.getElementById("result").style.color = "red";
+        document.getElementById("result").textContent = "Por favor, introduce valores válidos."
+    }
+}
